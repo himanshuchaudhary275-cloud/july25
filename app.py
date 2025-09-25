dccc489946d738678057b9bde8625b04f0235195
Deval Sandip Chaudhari <chaudhary.deval@gmail.com>
2025-09-25 14:27:23 +0530

    initial

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "hello"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+"""
+git init
+git add .
+git commit -m "initial"
+git remote add origin "your repository url"
+git push -u origin master
+"""
